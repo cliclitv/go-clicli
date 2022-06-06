118ed25a2817ed14250fd5d42810d8ec45c7d39d
yisar <1533540012@qq.com>
2022-06-06 19:12:35 +0800

    日常提交

--- fre/src/login/login.tsx
+++ fre/src/login/login.tsx
@@ -3,7 +3,7 @@ import { A, push } from '../use-route'
 import { post } from '../util/post'
 import './login.css'
 
-export default function Register() {
+export default function Login() {
     const [name, setName] = useState("")
     const [pwd, setPwd] = useState("")
 
@@ -16,8 +16,6 @@ export default function Register() {
     }
 
     function login() {
-        console.log(name, pwd)
-
         post("https://api.clicli.cc/user/login", { name, pwd }).then((res: any) => {
             if (res.code === 200) {
                 window.localStorage.setItem('token', res.token)
@@ -35,5 +33,17 @@ export default function Register() {
         <li><input type="text" placeholder="密码" onInput={(e) => changePwd(e.target.value)} /></li>
         <li><button onClick={login}>注册</button></li>
         <li><A href="/">注册</A></li>
+    </div>
+}
+
+
+export function Register() {
+    return <div class="login">
+        <li><h1>注册</h1></li>
+        <li><input type="text" placeholder="QQ（唯一标识）" /></li>
+        <li><input type="text" placeholder="昵称" /></li>
+        <li><input type="text" placeholder="密码" /></li>
+        <li><button>注册</button></li>
+        <li><A href="/login">登录</A></li>
     </div>
 }
