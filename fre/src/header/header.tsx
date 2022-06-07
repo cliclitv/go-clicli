2510608775fe43473b26d72a37b6e5bd3c05e5d7
yisar <1533540012@qq.com>
2022-06-07 13:18:42 +0800

    更换为unpkg

--- fre/src/header/header.tsx
+++ fre/src/header/header.tsx
@@ -17,7 +17,7 @@ export default function Header() {
                 </div>
                 <div className="biu">
                     <li><i className="icon-font icon-download"></i>Get APP</li>
-                    <li><i className="icon-font icon-upload" onClick={() => push('/upload')}></i>Upload</li>
+                    <li onClick={() => push('/upload')}><i className="icon-font icon-upload"></i>Upload</li>
                     <li className="avatar"><img src={getAvatar(user.qq)} alt="" /></li>
                 </div>
             </div>
