8666d6b333e38b991b52881b54420a6acfeb4543
yisar <1533540012@qq.com>
2022-11-27 19:44:08 +0800

    这波完美

--- fre/src/header/header.tsx
+++ fre/src/header/header.tsx
@@ -52,7 +52,7 @@ export default function Header() {
                             <h2>{parseInt(balance as any) / 1000000} 肥皂</h2>
                             <li>UID {user?.id}</li>
                             <li>我的投搞</li>
-                            <li><a href="https://goerli-faucet.pk910.de/">挖矿领水</a></li>
+                            <li><a href="https://sepolia-faucet.pk910.de/">挖矿领水</a></li>
                             <li onClick={() => push(`/user/${(user || {}).qq}`)}>个人中心</li>
                         </div>}
                     </li>
