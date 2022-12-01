20468aaa8fa253829ca0d822442b1c6692fc9017
yisar <1533540012@qq.com>
2022-12-01 20:16:41 +0800

    emmm

--- fre/src/app.tsx
+++ fre/src/app.tsx
@@ -27,7 +27,7 @@ render(<App />, document.getElementById("app"))
 
 
 // 以下都是时间戳对比
-if (Date.now() < 1670256000000) {
+if (Date.now() < 1670256000000 && window.location.pathname === '/') {
     document.getElementById('app').style = `filter: grayscale(100%);position:absolute;top:0;bottom:0;left:0;right:0;`
 }
 
