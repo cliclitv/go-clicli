03f17c8c76af8f4f3967a42c6e0bd62f675eaec3
伊撒尔 <1533540012@qq.com>
2023-01-08 21:57:37 +0800

    Update user.go

--- handler/user.go
+++ handler/user.go
@@ -57,8 +57,7 @@ func Login(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 		sendMsg(w, 500, fmt.Sprintf("%s", err))
 		return
 	}
-	fmt.Println("%x", resp)
-	if pwd != resp.Pwd {
+	if resp == nil || pwd != resp.Pwd {
 		sendMsg(w, 400, "用户名或密码错误")
 		return
 	} else {
