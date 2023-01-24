658248397c61ca22b100d15743553c1a5e2044dd
yisar <1533540012@qq.com>
2023-01-24 20:56:45 +0800

    日常提交

--- fre/src/upload/upload.tsx
+++ fre/src/upload/upload.tsx
@@ -125,7 +125,7 @@ export default function Upload(props) {
                     <option value="新番">新番</option>
                     <option value="完结">完结</option>
                     <option value="剧场版">剧场版</option>
-                    <option value="直播推流">直播推流</option>
+                    <option value="推流">推流</option>
                     <option value="影视">影视</option>
                 </select>
                 {props.id > 0 && <input type="text" value={post.time} onInput={e => change('time', e.target.value)} />}
