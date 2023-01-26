8699a6b74a6f3540b4fce92e0eee16fca3a95989
伊撒尔 <1533540012@qq.com>
2023-01-27 00:22:32 +0800

    更换cdn

--- fre/src/util/avatar.ts
+++ fre/src/util/avatar.ts
@@ -6,7 +6,7 @@ export function getAvatar(avatar) {
     return `https://q1.qlogo.cn/g?b=qq&nk=${avatar}&s=640`
   } else {
     let hash = md5(avatar)
-    return `https://gravatar.loli.net/avatar/${hash}?s=100&d=retro`
+    return `https://cdn.sep.cc/avatar/${hash}?s=100&d=retro`
   }
 }
 
