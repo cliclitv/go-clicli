f6e00fe75588bfe07f1b13ccc44c94137e778739
yisar <1533540012@qq.com>
2023-02-05 23:35:54 +0800

    日常提交

--- fre/src/header/footer.tsx
+++ fre/src/header/footer.tsx
@@ -1,7 +1,8 @@
-import {h} from 'fre'
+import { h } from 'fre'
 
-export default function Footer (props){
+export default function Footer(props) {
     return <footer>
-        <p>© 2016 - 2023 clicli.cc & CliCli Team <a href="https://github.com/cliclitv">Github</a></p>
+        <p>© 2016 - 2023 clicli.cc & CliCli Team <a href="https://github.com/cliclitv" class='github'>&nbsp;Github.&nbsp;&nbsp; </a> Cdn由<a href="asia-cdn.netdun.net
+"><img src="https://cdn-us.imgs.moe/2023/02/05/63dfca2a315d8.png" alt="" /></a>赞助</p>
     </footer>
 }
