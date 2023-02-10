d913de279a6fe8aaaaf8be4583f936fcfb8bd0ad
yisar <1533540012@qq.com>
2023-02-10 22:42:08 +0800

    emmm

--- fre/src/search/search.tsx
+++ fre/src/search/search.tsx
@@ -16,24 +16,15 @@ export default function Home({k}) {
         }
         
     }, [])
-    const obj = {
-        "home": "Home",
-        "list": "Index",
-    }
+
     return (
         <div>
             <div className="wrap">
-                <nav>
-                    {Object.keys(obj).map(key => {
-                        return <li><i className={`icon-font icon-${key}`}></i>{obj[key]}</li>
-                    })}
-                </nav>
                 <main>
                     <h1>Search Videos</h1>
                     <ListA posts={search} />
                 </main>
             </div>
-
         </div>
     )
 }
