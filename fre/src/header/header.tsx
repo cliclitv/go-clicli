78e7461ca491aeff7605984b06b6fe9c0d3133cb
yisar <1533540012@qq.com>
2023-02-13 20:04:23 +0800

    日常提交

--- fre/src/header/header.tsx
+++ fre/src/header/header.tsx
@@ -46,7 +46,7 @@ export default function Header() {
                     <a href="https://app.clicli.cc" target="_blank"><li><i className="icon-font icon-download"></i>APP</li></a>
                     <a href="#" onclick={()=>push('/login')}><li><i className="icon-font icon-denglu"></i>登录</li></a>
                     <li onClick={() => push('/upload/0')}><i className="icon-font icon-upload"></i>投稿</li>
-                    {/* <li onClick={() => push('/vip')}><i className="icon-font icon-dadou"></i>弯豆</li> */}
+                    <li onClick={() => push('/vip')}><i className="icon-font icon-dadou"></i>弯豆</li>
                     <li onClick={openWallet} style={{position:'relative'}}>
                     <Avatar uqq={user.qq} uname={user.name} utime={user.time} />
                         <div className="wallet" style={{ display: bal ? 'block' : 'none' }}>
