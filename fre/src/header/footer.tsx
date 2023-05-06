83e3450c0de783b301f070051c01a9cd985dd7ed
unknown <1533540012@qq.com>
2023-05-06 17:18:42 +0800

    播放页改版

--- fre/src/header/footer.tsx
+++ fre/src/header/footer.tsx
@@ -2,7 +2,12 @@ import { h } from 'fre'
 
 export default function Footer(props) {
     return <footer>
-        <p>© 2016 - 2023 clicli.cc & CliCli Team <a href="https://github.com/cliclitv" class='github'>&nbsp;Github.&nbsp;&nbsp; </a> Cdn由<a href="https://netdun.net
+        <h1>最佳体验，尽在 CliCli APP </h1>
+        <ul>
+            <a href="https://app.clicli.cc" target="_blank"><li>马上下载</li></a>
+
+        </ul>
+        <p>© 2016 - 2023 clicli.cc & CliCli Team <a href="https://github.com/cliclitv" class='github'>&nbsp;Github.&nbsp;&nbsp; </a> CDN 由<a href="https://netdun.net
 "><img src="https://cdn-us.imgs.moe/2023/02/05/63dfca2a315d8.png" alt="" /></a>赞助</p>
     </footer>
 }
