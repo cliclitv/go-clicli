4afa324edd4baf278fb8bc764d50cbd4d391358d
unknown <1533540012@qq.com>
2023-05-25 21:45:11 +0800

    修复bug

--- fre/src/swiper/swiper.tsx
+++ fre/src/swiper/swiper.tsx
@@ -10,16 +10,20 @@ export default function Swiper() {
     const [list1, setList1] = useState([])
     const [list2, setList2] = useState([])
     useEffect(() => {
-
         getPost('', '独播', 1, 6).then((res: any) => {
-            setList1(res.posts.slice(0,3))
-            setList2(res.posts.slice(3,6))
-            setTimeout(()=>{
-                const el = document.querySelector('.blaze-slider')
-                new window.BlazeSlider(el as any)
+            setList1(res.posts.slice(0, 3))
+            setList2(res.posts.slice(3, 6))
+            setTimeout(() => {
+                if (window.BlazeSlider) {
+                    const el = document.querySelector('.blaze-slider')
+                    new window.BlazeSlider(el as any)
+                }
             })
         })
     }, [])
+    useEffect(() => {
+        document.querySelectorAll('ul').innerHTML = ''
+    }, [list1, list2])
     return (
         <div class="blaze-slider">
             <div class="blaze-container">
@@ -27,15 +31,15 @@ export default function Swiper() {
                     <div class="blaze-track">
                         <div>
                             <ul>
-                                {list1.map(item=>{
-                                    return <li key={item.id}><img src={getSuo(item.content)} onClick={() => push(`/play/gv${item.id}`)}/><p>{item.title}</p></li>
+                                {list1.map(item => {
+                                    return <li key={item.id}><img src={getSuo(item.content)} onClick={() => push(`/play/gv${item.id}`)} /><p>{item.title}</p></li>
                                 })}
                             </ul>
                         </div>
                         <div>
                             <ul>
-                            {list2.map(item=>{
-                                    return <li key={item.id}><img src={getSuo(item.content)} onClick={() => push(`/play/gv${item.id}`)}/><p>{item.title}</p></li>
+                                {list2.map(item => {
+                                    return <li key={item.id}><img src={getSuo(item.content)} onClick={() => push(`/play/gv${item.id}`)} /><p>{item.title}</p></li>
                                 })}
                             </ul>
                         </div>
