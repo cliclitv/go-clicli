5fb1a9a9f8ce31438cff7c1faa7baef3555e9bff
unknown <1533540012@qq.com>
2023-05-25 13:28:59 +0800

    添加谷歌广告

--- fre/src/header/footer.tsx
+++ fre/src/header/footer.tsx
@@ -7,6 +7,9 @@ export default function Footer(props) {
             <a href="https://app.clicli.cc" target="_blank"><li>马上下载</li></a>
 
         </ul>
+        <div>
+            
+        </div>
         <p>© 2016 - 2023 clicli.cc & CliCli Team <a href="https://github.com/cliclitv" class='github'>&nbsp;Github.&nbsp;&nbsp; </a> CDN 由<a href="https://netdun.net
 "><img src="https://cdn-us.imgs.moe/2023/02/05/63dfca2a315d8.png" alt="" /></a>赞助</p>
     </footer>
