b8137d182b80b0cc34604952aaa1f5b99b0b4650
unknown <1533540012@qq.com>
2023-06-17 21:48:47 +0800

    简化逻辑

--- tm/src/header/header.tsx
+++ tm/src/header/header.tsx
@@ -1,4 +1,4 @@
-import { render, useState, h } from "fre"
+import { render, useState, h, Fragment } from "fre"
 import { push } from '../use-route'
 import { getUser, getBal } from "../util/api"
 import './header.css'
@@ -34,16 +34,14 @@ export default function Header() {
                     {/* <input type="text" placeholder="搜一下下菊花又不会坏😏" onKeyDown={keydown} onInput={(e) => changeKey(e.target.value)} /> */}
                 </div>
                 <div className="biu">
-                    <a href="https://app.tm0.net" target="_blank"><li><i className="icon-font icon-download"></i>APP</li></a>
-                    {user.id == null ? <a href="#" onclick={() => push('/login')}><li><i className="icon-font icon-denglu"></i>登录</li></a>:<a></a>}
                     <li onClick={() => push('/recharge')}><i className="icon-font icon-dadou"></i>弯豆</li>
                     <ul onClick={openWallet} style={{ position: 'relative' }}>
                         <Avatar uqq={user.qq} uname={user.name} utime={user.time} />
                         <div className="wallet" style={{ display: bal ? 'block' : 'none' }}>
-                            <li>UID {user?.id}</li>
+                            {user.id == null ? <li onclick={() => push('/login')}><i className="icon-font icon-denglu"></i>登录</li> : (<><li>UID {user?.id}</li>
                             <li onClick={() => push('/publish/0')}>投稿</li>
                             <li onClick={() => push(`/my/${(user || {}).id}`)}>我的投搞</li>
-                            <li onClick={() => push(`/user/${(user || {}).qq}`)}>个人中心</li>
+                            <li onClick={() => push(`/user/${(user || {}).qq}`)}>个人中心</li></>)}
                         </div>
                     </ul>
 
