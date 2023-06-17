a0f5771004e92b0ce55eda64baf999943d2c2149
unknown <1533540012@qq.com>
2023-06-17 20:38:17 +0800

    日常提交

--- tm/src/app.tsx
+++ tm/src/app.tsx
@@ -11,7 +11,7 @@ const routes = {
     '/register': import('./login/register'),
     '/publish/:id': import('./tm-upload/upload'),
     '/search/:k': import('./search/search'),
-    '/book/:pid':import('./book/book'),
+    '/watch/:pid':import('./book/book'),
     '/user/:id': import('./login/register'),
     '/recharge': import('./recharge/recharge'),
     '/my/:id': import('./admin/admin'),
