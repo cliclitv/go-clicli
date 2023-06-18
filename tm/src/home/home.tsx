61ae3928a10340cad8a500fa774b62be1677f740
unknown <1533540012@qq.com>
2023-06-18 21:57:29 +0800

    日常提交

--- tm/src/home/home.tsx
+++ tm/src/home/home.tsx
@@ -36,7 +36,7 @@ export default function Home() {
                 {/* <img src="https://cdn-us.imgs.moe/2023/06/17/648d4af9bc8be.png" alt="" class="logo" /> */}
                 <img src="https://cdn-us.imgs.moe/2023/06/17/648d5c210f99c.png" alt="" />
             </div>
-            <a href="https://npm.elemecdn.com/bcy-app@latest/web/app-release.apk"><button>下载</button></a>
+            <a href="https://npm.elemecdn.com/bcy-app@0.0.2-beta1/web/app-release.apk"><button>下载</button></a>
             <div>投稿 & 数据迁移请加群：863417519</div>
             <div>发布页地址：https://fanfic.com.cn</div>
         </main>
