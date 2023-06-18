244002dacb5b8ae09a86addda3f8c192cd75e929
unknown <1533540012@qq.com>
2023-06-18 11:16:49 +0800

    日常提交

--- tm/src/home/home.tsx
+++ tm/src/home/home.tsx
@@ -31,12 +31,12 @@ export default function Home() {
     return (
         <main class="wrap main">
             <h1>Fanfic</h1>
-            <h2>先定个小目标，半次元平替</h2>
+            <h2>一款由于半次元停服而产生的APP</h2>
             <div>
                 {/* <img src="https://cdn-us.imgs.moe/2023/06/17/648d4af9bc8be.png" alt="" class="logo" /> */}
                 <img src="https://cdn-us.imgs.moe/2023/06/17/648d5c210f99c.png" alt="" />
             </div>
-            <a href="https://npm.elemecdn.com/bcy-app@latest/web/app-release.apk"><button>下载</button></a>
+            <a href="https://npm.elemecdn.com/bcy-app@0.0.1-beta3/web/app-release.apk"><button>下载</button></a>
             <div>投稿 & 数据迁移请加群：863417519</div>
             <div>发布页地址：https://fanfic.com.cn</div>
         </main>
