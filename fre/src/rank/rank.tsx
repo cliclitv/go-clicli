f59b92016cd6034a5849c45db10bf8b74f38c85d
unknown <1533540012@qq.com>
2023-09-03 15:40:37 +0800

    修改完成

--- fre/src/rank/rank.tsx
+++ fre/src/rank/rank.tsx
@@ -7,13 +7,22 @@ import './rank.css'
 
 export default function Rank(props) {
     const [posts, setPosts] = useState([])
+    const [day, setDay] = useState(900)
     useEffect(() => {
-        getRank(30).then((res: any) => {
+        getRank(day).then((res: any) => {
             setPosts(res.posts)
         })
-    }, [])
+    }, [day])
     return <div className="rank">
-        <h1>排行榜</h1>
+        <div class="top">
+            <h1>排行榜</h1>
+            <ul>
+                <li onclick={() => setDay(900)} class={day === 900 ? 'active' : ''}>总榜</li>
+                <li onclick={() => setDay(300)} class={day === 300 ? 'active' : ''}>年榜</li>
+                <li onclick={() => setDay(100)} class={day === 100 ? 'active' : ''}>季榜</li>
+                <li onclick={() => setDay(30)} class={day === 30 ? 'active' : ''}>月榜</li>
+            </ul>
+        </div>
         <ul>
             {posts.length > 0 && posts.map((item, index) => {
                 return index === 0 ?
@@ -32,8 +41,8 @@ export default function Rank(props) {
                     :
                     <li key={item.id} onClick={() => push(`/play/gv${item.id}`)}>
                         <span className={index < 3 ? 'active' : ''}>{index + 1}</span>
-                        
-                            <div className='title'>{item.title}</div>
+
+                        <div className='title'>{item.title}</div>
                     </li>
             })}
         </ul>
