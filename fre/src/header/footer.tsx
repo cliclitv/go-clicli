8c6eadc8eee627a4d4c2ef235426685db1ea598e
unknown <1533540012@qq.com>
2023-09-13 14:58:23 +0800

    更新广告

--- fre/src/header/footer.tsx
+++ fre/src/header/footer.tsx
@@ -1,12 +1,15 @@
 export default function Footer(props) {
     return <footer class='wrap'>
+        <div class="ad">
+            <script src="//js.penxiangge.com/inc/s.php?s=72328&w=760&h=60"></script>
+        </div>
         <p>© 2018 - 2023 clicli.cc & admin@clicli.us <a href="https://github.com/cliclitv" class='github'>Github</a></p>
 
         <ul class="friends">
-        
-        <a href="https://tempstsuma.cn/" class='github' target="_blank">友链(Tempstsuma)</a>
-        <a href="https://www.summerpockets.com" class='github' target="_blank">友链(鸟白岛演绎厅)</a>
-        <a href="https://www.huahuo6.com" class='github' target="_blank">友链(花火轻小说)</a>
+
+            <a href="https://tempstsuma.cn/" class='github' target="_blank">友链(Tempstsuma)</a>
+            <a href="https://www.summerpockets.com" class='github' target="_blank">友链(鸟白岛演绎厅)</a>
+            <a href="https://www.huahuo6.com" class='github' target="_blank">友链(花火轻小说)</a>
         </ul>
     </footer>
 }
