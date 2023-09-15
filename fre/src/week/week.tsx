c0ebfe7379cb5a8994cfa5e3137bbf880acb6633
unknown <1533540012@qq.com>
2023-09-15 14:36:43 +0800

    增加飞机杯

--- fre/src/week/week.tsx
+++ fre/src/week/week.tsx
@@ -1,4 +1,4 @@
-import {  useEffect, useState } from 'fre'
+import { useEffect, useState } from 'fre'
 import { push } from '../use-route'
 import { getPost } from '../util/api'
 import { getSuo } from '../util/avatar'
@@ -37,19 +37,29 @@ export default function WeekList() {
                         onClick={() => setDay(index)}>{map[item]}</button>)}
                 </ul>
             </div>
-            <ul className="posts">
-                {posts[day] ? posts[day].map((item, index) => (
-                    <li key={index} onClick={() => push(`/play/gv${item.id}`)}>
-                        <div className="post">
-                            <div className="cover">
-                                <img src={getSuo(item.content)} />
+            <div class="weekcontent">
+                <ul className="posts">
+                    {posts[day] ? posts[day].map((item, index) => (
+                        <li key={index} onClick={() => push(`/play/gv${item.id}`)}>
+                            <div className="post">
+                                <div className="cover">
+                                    <img src={getSuo(item.content)} />
+                                </div>
+                                <div className="title">{item.title}</div>
                             </div>
-                            <div className="title">{item.title}</div>
-                        </div>
-                    </li>
-                )
-                ) : <div></div>}
-            </ul>
+                        </li>
+                    )
+                    ) : <div></div>}
+                </ul>
+                <a href="https://shop119340084.taobao.com/shop/view_shop.htm?spm=a230r.1.14.4.75dc14ecGDLY4r&user_number_id=1965847533&mm_sycmid=1_143947_3565a75377635dc11b2616c8c6d51a7d" target="_blank">
+                    <span>官方赞助</span>
+                    <img src="http://image.planet.youku.com/img/100/15/47949/i_1694758247949_203bbf08698d4b1e4d749e42c3e20be3_b_w1600h1600.jpg" alt="" />
+                    <div>
+                        <p>哥哥^_^打个胶叭！</p>
+                        <button>暗号CLI</button>
+                    </div>
+                </a>
+            </div>
         </div>
     </div>
 }
