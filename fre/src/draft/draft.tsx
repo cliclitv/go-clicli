a71f0dce27bbdeb049d221c1b75bfb8857c3f1da
yisar <1533540012@qq.com>
2024-02-07 19:23:56 +0800

    feat: 搞定

--- fre/src/draft/draft.tsx
+++ fre/src/draft/draft.tsx
@@ -90,12 +90,11 @@ export default function Upload(props) {
         let myWindow = window.open(url, '', 'width=800,height=600,toolbar=no, menubar=no, scrollbars=no, resizeable=no, location=0, status=no')
         myWindow.focus()
     }
-    const tags = ['推荐', '个人原创', '授权转载', '国漫', '剧场版', '漫画改', '小说改', '游戏改', '耽美', '乙女', '百合', '后宫', '热血', '战斗', '运动', '奇幻', '神魔', '治愈',
+    const tags = ['推荐', '幻灯', '个人原创', '授权转载', '国漫', '剧场版', '漫画改', '小说改', '游戏改', '耽美', '乙女', '百合', '后宫', '热血', '战斗', '运动', '奇幻', '神魔', '治愈',
         '搞笑', '冒险', '校园', '恐怖', '穿越', '推理', '科幻', '日常', '古风', '恋爱', 'r15', '泡面番', '黄金厕纸',
-        '特摄', '真人剧', '其它', '幻灯']
-    const gametags = [
-        '鬼畜', 'AMV/MAD', '音乐·PV', '游戏·GMV', 'VOCALOID',
-        '原神', '星穹铁道', '崩坏三', '明日方舟', '火影忍者', '三国杀', '绝区零', '反恐精英', '英雄联盟', '王者荣耀', '塞尔达', '碧蓝航线', '鸣潮', '无畏契约', '我的世界', '其他原创', '小程序', '小游戏'
+        '特摄', '真人剧', '其它']
+    const acgzoneTags = [
+        '推荐', '幻灯', '漫画', '动画', '游戏', '小说', '图包', '音乐', '三次元'
     ]
     return (
         <div className="wrap flex">
@@ -114,7 +113,10 @@ export default function Upload(props) {
                     <i class="te te-upload" onclick={() => openWindow(`https://cdn.clicli.cc/upload?uid=${user.id}`)}></i>
                 </section>
                 <textarea spellcheck="false" placeholder="请输入简介，支持 markdown 语法" value={post.content} onInput={e => change('content', e.target.value)}></textarea>
-                <textarea spellcheck="false" placeholder={`直链框，请输入标题+$+直链，如：第一话$https://clicli.cc/001.mp4\n多个分P用回车隔开`} value={post.videos} class="videos" onInput={e => change('videos', e.target.value)}></textarea>
+                <textarea spellcheck="false" placeholder={post.sort === '里世界'
+                    ? `直链框，请输入标题+$+直链，如：第一话$https://clicli.cc/001.mp4\n多个分P用回车隔开` :
+                    `图集框，请输入图床地址，如：https://clicli.cc/001.png\n多图流用回车隔开`
+                } value={post.videos} class="videos" onInput={e => change('videos', e.target.value)}></textarea>
 
                 <div className="options">
                     <select onInput={e => change('status', e.target.value)}>
@@ -126,16 +128,13 @@ export default function Upload(props) {
                     <select onInput={e => change('sort', e.target.value)}>
                         <option value="新番" selected={post.sort === '新番'}>新番</option>
                         <option value="完结" selected={post.sort === '完结'}>完结</option>
-                        <option value="推流" selected={post.sort === '推流'}>推流</option>
-                        <option value="原创" selected={post.sort === '原创'}>原创</option>
-                        <option value="漫画解说" selected={post.sort === '漫画解说'}>漫画解说</option>
-                        <option value="漫剧" selected={post.sort === '漫剧'}>漫剧</option>
+                        <option value="完结" selected={post.sort === '完结'}>里世界</option>
                     </select>
                     {props.id > 0 && <input type="text" value={post.time} onInput={e => change('time', e.target.value)} />}
                 </div>
                 <div className="tags">
                     <ul>
-                        {(post.sort === '原创' ? gametags : tags).map((item, index) => <li onClick={() => selectTag(item)} key={index.toString()}
+                        {(post.sort === '里世界' ? acgzoneTags : tags).map((item, index) => <li onClick={() => selectTag(item)} key={index.toString()}
                             className={(post.tag || '').indexOf(item) > -1 ? 'active' : ''}>{item}</li>)}
                     </ul>
 
