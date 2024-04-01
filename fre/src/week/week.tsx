8562a47a5eb9e91d3d14ca7dd8f6b0c514b858f4
yisar <1533540012@qq.com>
2024-04-01 08:48:58 +0800

    emmm

--- fre/src/week/week.tsx
+++ fre/src/week/week.tsx
@@ -52,9 +52,9 @@ export default function WeekList() {
                     ) : <div></div>}
                 </ul>
                 <div className="ad">
-                    <a href="https://shop119340084.taobao.com/shop/view_shop.htm?spm=a230r.1.14.4.75dc14ecGDLY4r&user_number_id=1965847533&mm_sycmid=1_143947_3565a75377635dc11b2616c8c6d51a7d" target="_blank">
+                    <a href="https://shop.m.taobao.com/shop/shop_index.htm?sellerId=1965847533&shopId=119340084&inShopPageId=413203121&pathInfo=shop/index2&mm_sycmid=1_149789_b2afceece31d3a0f7034537f8cadc8b2" target="_blank">
                         <span>官方赞助</span>
-                        <img src="http://image.planet.youku.com/img/100/15/47949/i_1694758247949_203bbf08698d4b1e4d749e42c3e20be3_b_w1600h1600.jpg" alt="" />
+                        <img src="https://ice.frostsky.com/2024/04/01/6d16b318ac3766b0d06546998c5066ed.jpeg" alt="" />
                         <div>
                             <p>哥哥^_^打个胶叭！</p>
                             <button>暗号CLI</button>
