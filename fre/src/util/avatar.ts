b27a7e945bc5c49f39bdee2f78034cb51ad2439b
yisar <1533540012@qq.com>
2024-06-10 16:13:11 +0800

    更换cdn

--- fre/src/util/avatar.ts
+++ fre/src/util/avatar.ts
@@ -6,7 +6,7 @@ export function getAvatar(avatar) {
     return `https://q1.qlogo.cn/g?b=qq&nk=${avatar}&s=640`
   } else {
     let hash = md5(avatar)
-    return `https://cdn.sep.cc/avatar/${hash}?s=100&d=retro`
+    return `https://gravatar.aby.pub/avatar/${hash}?s=100`
   }
 }
 
