15ce71adceb8d6f6e03795b699d5a6df2fea072b
unknown <1533540012@qq.com>
2024-07-10 22:24:08 +0800

    日常提交

--- fre/src/util/api.ts
+++ fre/src/util/api.ts
@@ -99,12 +99,12 @@ export function getComments(pid, rid, page?, pageSize?) {
   return get(`https://www.clicli.cc/comments?pid=${pid}&rid=${rid}&page=${page || 1}&pageSize=${pageSize || 1000}`)
 }
 
-export function addComment({ pid, uid, pos, content, rid = 0, ruid, read = 0 }) {
+export function addComment({ pid, uid, pos, content, rid = 0, ruid, runame }) {
   return post('https://www.clicli.cc/comment/add', {
     content,
     pos: pos.toString(),
     uid: getUser().id,
-    pid, rid, ruid, read
+    pid, rid, ruid, runame
   })
 }
 
