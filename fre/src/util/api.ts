ec94553295e27840dbc7496d83f3ab62b172f72b
unknown <1533540012@qq.com>
2024-07-10 22:39:37 +0800

    emmm

--- fre/src/util/api.ts
+++ fre/src/util/api.ts
@@ -99,12 +99,12 @@ export function getComments(pid, rid, page?, pageSize?) {
   return get(`https://www.clicli.cc/comments?pid=${pid}&rid=${rid}&page=${page || 1}&pageSize=${pageSize || 1000}`)
 }
 
-export function addComment({ pid, uid, pos, content, rid = 0, ruid, rstr }) {
+export function addComment({ pid, uid, pos, content, rid = 0, rstr }) {
   return post('https://www.clicli.cc/comment/add', {
     content,
     pos: pos.toString(),
     uid: getUser().id,
-    pid, rid, ruid, rstr
+    pid, rid, rstr
   })
 }
 
